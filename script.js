2e963448c090a9fabbaff286ebc4c7f003d847eb
Tess Huelskamp <tess@huelskamp.io>
2022-05-09 11:48:11 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".red").click(function(){
+    $(".result").text("red!");
+    $(".result").css("color", "red");
+   
+});
+
+$(".green").click(function(){
+    $(".result").text("green!");
+    $(".result").css("color", "green");
+   
+});
+
+
+$(".blue").click(function(){
+    $(".result").text("blue!");
+    $(".result").css("color", "blue");
+});
+
+
